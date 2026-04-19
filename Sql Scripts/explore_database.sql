f6706840b52ca3d23a320cc83aece808bbe75374
Anubhav Saxena <saxenaanubhav74@gmail.com>
2026-04-19 22:08:02 +0530

    Delete Sql Scripts directory

--- Sql Scripts/explore_database.sql
+++
@@ -1,7 +0,0 @@
--- Explore All onjects in the Database
-
-SELECT * FROM INFORMATION_SCHEMA.TABLES
-
--- Explore All Columns in the Database
-SELECT * FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_NAME = 'dim_customers'
